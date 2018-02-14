cf8315a60a42b9a1a5120e2d7e487264b1826a60
Eric Alter <neolanders@hotmail.com>
2018-02-13 23:04:43 -0800

    Update App Title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Movies List'`, async(() => {
+  it(`should have as title 'Popular Movies on TMDb`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Movies List');
+    expect(app.title).toEqual('Popular Movies on TMDb');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Movies List');
+    expect(compiled.querySelector('h1').textContent).toContain('Popular Movies on TMDb');
   }));
   it('should contain the app-movies-filter components', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
